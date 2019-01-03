9087df8cab5f607ac8c450fff3b78ee61be50e1f
Tyler Huang <thuang00@stuy.edu>
2019-01-03 11:42:52 -0500

    created-block-class

---
+++ block.java
@@ -0,0 +1,12 @@
+public class block{
+
+  int xcor, ycor;
+
+  /**constructor
+    *@param x,y are the x-coordinates and y-coordinates of the block
+    **/
+  public block(int x, int y){
+    xcor = x;
+    ycor = y;
+  }
+}
