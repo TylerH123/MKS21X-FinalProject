0a0ec981e70f00584687835218ee9f8a8f432a44
Jude Rizzo <jrizzo00@stuy.edu>
2019-01-04 14:11:05 -0700

    added Block functions, made a piece and tetris file

--- block.java
+++ block.java
@@ -1,12 +1,26 @@
-public class block{
+public class Block{
 
-  int xcor, ycor;
+  private int xcor, ycor;
 
   /**constructor
     *@param x,y are the x-coordinates and y-coordinates of the block
     **/
-  public block(int x, int y){
+  public Block(int x, int y){
     xcor = x;
     ycor = y;
   }
+  //Implementing the bastic methods movdown getx and gety
+  public void moveDown(){
+    xcor -= 1;
+  }
+
+  public int getX(){
+    return xcor
+  }
+
+  public int getY(){
+    return ycor;
+  }
+
+  
 }
