74cb40d6029c1abaa83662407a754a7f343416d2
Tyler Huang <thuang00@stuy.edu>
2019-01-04 11:39:39 -0500

    added some comments

--- cube.java
+++ cube.java
@@ -1,4 +1,18 @@
 public class cube implements fourBlock {
 
-  
+  /**constructor
+    *@param block a,b,c,d are used to create the cube
+    **/
+  public cube(block a, block b, block c, block d){
+
+  }
+  public void moveRight(){};
+  public void moveLeft(){};
+  public void descend(){};
+  public void rotateRight(){};
+  public void rotateLeft(){};
+  public String toString(){
+    return "";
+  };
+
 }
