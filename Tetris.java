7f4e530c2743cc1c0f606041035d48428fbede1f
Jude Rizzo <jrizzo00@stuy.edu>
2019-01-07 11:46:54 -0500

    Trying to make board

--- Tetris.java
+++ Tetris.java
@@ -1,4 +1,21 @@
 import java.util.*;
+import com.googlecode.lanterna.terminal.Terminal.SGR;
+import com.googlecode.lanterna.TerminalFacade;
+import com.googlecode.lanterna.input.Key;
+import com.googlecode.lanterna.input.Key.Kind;
+import com.googlecode.lanterna.terminal.Terminal;
+import com.googlecode.lanterna.terminal.Terminal.Color;
+import com.googlecode.lanterna.terminal.TerminalSize;
+import com.googlecode.lanterna.LanternaException;
+import com.googlecode.lanterna.input.CharacterPattern;
+import com.googlecode.lanterna.input.InputDecoder;
+import com.googlecode.lanterna.input.InputProvider;
+import com.googlecode.lanterna.input.Key;
+import com.googlecode.lanterna.input.KeyMappingProfile;
+
+
+
+
 public class Tetris{
   private int height, width;
   private boolean[][] pieces;
@@ -6,6 +23,7 @@ public class Tetris{
   //This can be a 1D version of a 2D array of all the blocks. The coordinates is x = index% width y = index/width
   private int score;
   private boolean running = true;
+  private TerminalSize size;
   //Blocks is an array of all the coordinates in the tetris board, each index in the 2d array representaing
   //a coordinate. False means there is no block on the piece while
   public boolean isRunning(){
@@ -22,10 +40,18 @@ public class Tetris{
         pieces[i][j] = false;
       }
     }
+    size = terminal.getTerminalSize();
+    terminal.setCursorVisible(true);
+    size.setColumns(10);
+    size.setRows(24);
   }
 
-  public printBoard(){
-    
+  TerminalSize size = terminal.getTerminalSize();
+  terminal.setCursorVisible(true);
+  size.setColumns(10);
+  size.setRows(24);
+  public void printBoard(){
+
   }
 
     //Note that these coordinates are (row column), which are (y, x), not (x, y)
