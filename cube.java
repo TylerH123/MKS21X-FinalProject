d8c623e92f15e4e56195cda158aa52f3d1d2be4d
Tyler Huang <thuang00@stuy.edu>
2019-01-07 11:43:23 -0500

    fixed cube class constructor

--- cube.java
+++ cube.java
@@ -3,8 +3,11 @@ public class cube implements fourBlock {
   /**constructor
     *@param block a,b,c,d are used to create the cube
     **/
-  public cube(block a, block b, block c, block d){
-
+  public cube(int maxHeight, int midWidth){
+    block a = new block(midWidth,maxHeight);
+    block b = new block(midWidth+1,maxHeight-1);
+    block c = new block(midWidth,maxHeight-1);
+    block d = new block(midWidth+1,maxHeight);
   }
   public void moveRight(){};
   public void moveLeft(){};
