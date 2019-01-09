7f80fe490d93556ec24d5653e7ccb81803ed348a
Tyler Huang <thuang00@stuy.edu>
2019-01-08 21:11:40 -0500

    added rightL class

---
+++ rightL.java
@@ -0,0 +1,42 @@
+public class rightL implements fourBlock{
+
+  block a,b,c,d;
+/**constructor
+  *@param maxHeight is the maximum hieght of the board
+  *@param midWidth is half of the width of the board
+  *creates the reverse L shaped piece using 4 blocks placed accordingly
+  **/
+public cube(int maxHeight, int midWidth){
+  a = new block(midWidth,maxHeight);
+  b = new block(midWidth+1,maxHeight-1);
+  c = new block(midWidth,maxHeight-1);
+  d = new block(midWidth+2,maxHeight-1);
+}
+//makes all the blocks in the reverse L move right
+public void moveRight(){
+  a.moveRight();
+  b.moveRight();
+  c.moveRight();
+  d.moveRight();
+}
+//makes all the blocks in the reverse L move left
+public void moveLeft(){
+  a.moveLeft();
+  b.moveLeft();
+  c.moveLeft();
+  d.moveLeft();
+}
+//makes all the blocks in the reverse L move down
+public void descend(){
+  a.moveDown();
+  b.moveDown();
+  c.moveDown();
+  d.moveDown();
+}
+//
+public void rotateRight(){
+}
+public void rotateLeft(){
+
+
+}
