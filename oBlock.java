e85dc1e1c5315018507f704b975b81aebfa78367
TylerH123 <33766086+TylerH123@users.noreply.github.com>
2019-01-08 21:28:57 -0500

    Update and rename cube.java to oBlock.java
    renamed file

--- oBlock.java
+++ oBlock.java
@@ -1,32 +1,32 @@
-public class cube implements fourBlock {
+public class oBlock implements fourBlock {
 
     block a,b,c,d;
   /**constructor
     *@param maxHeight is the maximum hieght of the board
     *@param midWidth is half of the width of the board
-    *creates the cube piece using 4 blocks placed accordingly
+    *creates the O-shaped piece using 4 blocks placed accordingly
     **/
-  public cube(int maxHeight, int midWidth){
+  public oBlock(int maxHeight, int midWidth){
     a = new block(midWidth,maxHeight);
     b = new block(midWidth+1,maxHeight-1);
     c = new block(midWidth,maxHeight-1);
     d = new block(midWidth+1,maxHeight);
   }
-  //makes all the blocks in the cube move right
+  //makes all the blocks in the O-shaped piece move right
   public void moveRight(){
     a.moveRight();
     b.moveRight();
     c.moveRight();
     d.moveRight();
   }
-  //makes all the blocks in the cube move left
+  //makes all the blocks in the O-shaped piece move left
   public void moveLeft(){
     a.moveLeft();
     b.moveLeft();
     c.moveLeft();
     d.moveLeft();
   }
-  //makes all the blocks in the cube move down
+  //makes all the blocks in the O-shaped piece move down
   public void descend(){
     a.moveDown();
     b.moveDown();
