625cf668c28be16bb24f6cac9360f305d35a72ea
TylerH123 <33766086+TylerH123@users.noreply.github.com>
2019-01-08 21:23:24 -0500

    Update and rename line.java to Lblock.java
    renamed file

--- Lblock.java
+++ Lblock.java
@@ -1,31 +1,31 @@
-public class line implements fourBlock{
+public class Lblock implements fourBlock{
   block a,b,c,d;
   /**constructor
     *@param maxHeight is the maximum hieght of the board
     *@param midWidth is half of the width of the board
-    *creates the line piece using 4 blocks placed in a line
+    *creates the L-shaped piece using 4 blocks placed in a line
   **/
-  public line(int maxHeight, int midWidth){
+  public Lblock(int maxHeight, int midWidth){
     a = new block(midWidth,maxHeight);
     b = new block(midWidth,maxHeight-1);
     c = new block(midWidth,maxHeight-2);
     d = new block(midWidth,maxHeight-3);
   }
-  //makes all the blocks in the cube move right
+  //makes all the blocks in the L-shaped piece move right
   public void moveRight(){
     a.moveRight();
     b.moveRight();
     c.moveRight();
     d.moveRight();
   }
-  //makes all the blocks in the cube move left
+  //makes all the blocks in the L-shaped piece move left
   public void moveLeft(){
     a.moveLeft();
     b.moveLeft();
     c.moveLeft();
     d.moveLeft();
   }
-  //makes all the blocks in the cube move down
+  //makes all the blocks in the L-shaped piece move down
   public void descend(){
     a.moveDown();
     b.moveDown();
