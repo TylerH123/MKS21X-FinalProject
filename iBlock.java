4f48b23f23efa86953c09c13e1fe54871cd20f56
TylerH123 <33766086+TylerH123@users.noreply.github.com>
2019-01-08 21:24:32 -0500

    Update and rename Lblock.java to iBlock.java
    renamed file

--- iBlock.java
+++ iBlock.java
@@ -1,31 +1,31 @@
-public class Lblock implements fourBlock{
+public class iBlock implements fourBlock{
   block a,b,c,d;
   /**constructor
     *@param maxHeight is the maximum hieght of the board
     *@param midWidth is half of the width of the board
-    *creates the L-shaped piece using 4 blocks placed in a line
+    *creates the I-shaped piece using 4 blocks placed in a line
   **/
-  public Lblock(int maxHeight, int midWidth){
+  public iBlock(int maxHeight, int midWidth){
     a = new block(midWidth,maxHeight);
     b = new block(midWidth,maxHeight-1);
     c = new block(midWidth,maxHeight-2);
     d = new block(midWidth,maxHeight-3);
   }
-  //makes all the blocks in the L-shaped piece move right
+  //makes all the blocks in the I-shaped piece move right
   public void moveRight(){
     a.moveRight();
     b.moveRight();
     c.moveRight();
     d.moveRight();
   }
-  //makes all the blocks in the L-shaped piece move left
+  //makes all the blocks in the I-shaped piece move left
   public void moveLeft(){
     a.moveLeft();
     b.moveLeft();
     c.moveLeft();
     d.moveLeft();
   }
-  //makes all the blocks in the L-shaped piece move down
+  //makes all the blocks in the I-shaped piece move down
   public void descend(){
     a.moveDown();
     b.moveDown();
