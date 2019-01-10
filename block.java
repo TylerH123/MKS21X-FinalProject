6648a70426a93991ee972c4df064d066df6e066b
Tyler Huang <thuang00@stuy.edu>
2019-01-09 20:40:24 -0500

    finished rotation for block class

--- block.java
+++ block.java
@@ -1,8 +1,13 @@
 public class block{
 
-  private int xcor, ycor;
-  private String left,right;
-  private String[] direction = new String[]{"botton", "right", "top", "left"};
+  //rightIdx is the index in direction thatright variable uses
+  //leftIdx is the index of direction that left variable uses
+  private int xcor, ycor, leftIdx, rightIdx;
+  //left shows the direction if turnLeft happens
+  //right shows the direction if turnRight happens
+  //current is the current direction
+  private String left,right, current;
+  private String[] direction = new String[]{"down", "right", "up", "left"};
 
   /**constructor
     *@param x,y are the x-coordinates and y-coordinates of the block
@@ -10,6 +15,11 @@ public class block{
   public block(int x, int y){
     xcor = x;
     ycor = y;
+    current = "down";
+    rightIdx = 1;
+    leftIdx = 3;
+    left = direction[leftIdx];
+    right = direction[rightIdx];
   }
   //Implementing the bastic methods movedown getx and gety
   public void moveDown(){
@@ -39,10 +49,27 @@ public class block{
   public void moveLeft(){
     xcor--;
   }
-  public void rotateRight(){
-
+  public String currentDirec(){
+    return current;
+  }
+  public void turnRight(){
+    current = right;
+    if (rightIdx == 3){
+      rightIdx = 0;
+    }
+    else{
+      rightIdx++;
+    }
+    right = direction[rightIdx];
   }
   public void rotateLeft(){
-
+    current = right;
+    if (leftIdx == 0){
+      leftIdx = 3;
+    }
+    else{
+      leftIdx++;
+    }
+    left = direction[leftIdx];
   }
 }
